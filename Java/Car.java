44a506bc86e8185e0f0d62ba50683f00f4b5041c
PaolaDaft <andrepaolagd@gmail.com>
2023-03-15 16:20:18 -0600

    Creado objetos en java

--- Java/Car.java
+++ Java/Car.java
@@ -3,5 +3,9 @@ class Car {
     String license;
     String driver;
     Integer passenger;
+
+    void printDataCar(){
+        System.out.println("license: " + license + " Driver: " + driver);
+    }
     
 }
--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,16 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        Car car = new Car();
+        car.license = "PD15";
+        car.driver = "José Ubaldo";
+        car.passenger= 6;
+        car.printDataCar();
+
+        Car car2 = new Car();
+        car2.license = "DAFT25";
+        car2.driver = "Paola Josefa";
+        car2.passenger = 3;
+        car2.printDataCar();
     }
 }   
