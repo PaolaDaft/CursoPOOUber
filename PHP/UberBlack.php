27e141a0800260a12505069e764602a556c6a808
PaolaDaft <andrepaolagd@gmail.com>
2023-03-16 17:19:27 -0600

    Herencia PHP

--- PHP/UberBlack.php
+++ PHP/UberBlack.php
@@ -1,5 +1,14 @@
 <?php
+require_once('car.php')
 class UbarBlack extends Car{
-    
+    public $typeCarAccepted;
+    public $seatMaterial;
+
+    public function __construct($license, $driver, $typeCarAccepted, $seatMaterial){
+        parent::__construct($license, $driver);
+        $this->typeCarAccepted = $typeCarAccepted;
+        $this->seatMaterial = $seatMaterial;
+    }
+
 }
 ?>
