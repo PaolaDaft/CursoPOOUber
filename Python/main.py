bb56710d98978d35964c808d89d44e53422948ca
PaolaDaft <andrepaolagd@gmail.com>
2023-03-16 07:19:03 -0600

    Creado objetos|clases en Python

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,15 @@
+from car import Car 
+
 if __name__ == "__main__":
     print("HEYYYY")
+    car = Car()
+    car.license = "PD15"
+    car.driver = "José Ubaldo"
+    car.passenger= 6
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "DAFT25"
+    car2.driver = "Paola Josefa"
+    car2.passenger = 3
+    print(vars(car2))
