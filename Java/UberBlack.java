fd1e2834fae5129f8e8340648d3310cf4aa8c97e
PaolaDaft <andrepaolagd@gmail.com>
2023-03-16 15:55:28 -0600

    Herecia Java | crenado las clases de los tipos de Uber

---
+++ Java/UberBlack.java
@@ -0,0 +1,11 @@
+class UberBlack extends Car {
+    Map<String, ArrayList<String, Integer>> typeCarAccepted;
+    ArrayList<String> seatMaterial;
+
+    public UberBlack(String license, Account driver, Map<String, ArrayList<String, Integer>> typeCarAccepted, ArrayList<String> seatMaterial){
+        super(license, driver);
+        this.typeCarAccepted = typeCarAccepted;
+        this.seatMaterial = seatMaterial;
+    }
+    
+}
---
+++ Java/UberVan.java
@@ -0,0 +1,11 @@
+class UberVan extends Car {
+    Map<String, ArrayList<String, Integer>> typeCarAccepted;
+    ArrayList<String> seatMaterial;
+
+    public UberVan(String license, Account driver, Map<String, ArrayList<String, Integer>> typeCarAccepted, ArrayList<String> seatMaterial){
+        super(license, driver);
+        this.typeCarAccepted = typeCarAccepted;
+        this.seatMaterial = seatMaterial;
+    }
+    
+}
