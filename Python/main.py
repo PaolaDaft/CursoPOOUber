62431def1f84d6bd5e1835b165d021413088ca4d
PaolaDaft <andrepaolagd@gmail.com>
2023-03-17 08:20:00 -0600

    Herencia Python

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,6 @@
 from car import Car 
 from account import Account
+from uberX import UberX
 
 if __name__ == "__main__":
     print("HEYYYY")
@@ -7,3 +8,7 @@ if __name__ == "__main__":
     car = Car("PD15", Account("José Ubaldo", "JOUB16"))
     print(vars(car))
     print(vars(car.driver))
+
+    uberX = UberX("DAFT25", Account("Paola Josefa", "PAJO15"), "Dodge", "Attitude")
+    print(vars(uberX))
+    print(vars(uberX.driver))
