9c0d7201f987a736456d5d4d6d390f7b71cfd1fe
PaolaDaft <andrepaolagd@gmail.com>
2023-03-17 08:36:56 -0600

    Herencia JS

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("PD15", new Account("José Ubaldo", "JOUB16"))
 car.passanger = 4
 car.printDataCar()
+
+var uberX = new UberX("DAFT25", new Account("Paola Josefa", "PAJO15"), "Dodge", "Attitude")
+    uberX.passanger = 4
+    uberX.printDataCar()
