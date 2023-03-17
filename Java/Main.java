a5786225d68c5427b02013509a31e4938db9a3ce
PaolaDaft <andrepaolagd@gmail.com>
2023-03-17 09:31:04 -0600

    Encapsulamiento Java

--- Java/Main.java
+++ Java/Main.java
@@ -1,12 +1,12 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        Car car = new Car("PD15", new Account("José Ubaldo", "JOUB16"));
-        car.passenger= 6;
-        car.printDataCar();
+        UberX uberX = new UberX("PD15", new Account("José Ubaldo", "JOUB16"), "Chevrolet", "Sonic");
+        uberX.setPassenger(3);
+        uberX.printDataCar();
 
         Car car2 = new Car("DAFT25", new Account("Paola Josefa", "PAJO15"));
-        car2.passenger = 3;
+        //car2.passenger = 3;
         car2.printDataCar();
     }
 }   
